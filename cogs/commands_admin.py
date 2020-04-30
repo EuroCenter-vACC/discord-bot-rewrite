59fd37e5ac7e084f5e4c69261d74b48e39a19c58
Peter PARE <peter.pare.new@gmail.com>
2020-04-30 17:56:57 +0200

    update command

--- cogs/commands_admin.py
+++ cogs/commands_admin.py
@@ -1,24 +1,30 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 from configparser import ConfigParser
 config = ConfigParser()
 config.read("config.ini")
-nav = config.get("USERS", "nav_list").split(", ")
 server_id = int(config.get("GENERAL", "server_id"))
 
 class commandsAdmin(commands.Cog):
     
     def __init__(self, client):
         self.client = client
-        self.guild = self.client.get_guild(server_id)
 
     @commands.command(name="update", aliases=['Update', 'UPDATE'])
     async def update(self, ctx, *, args):
-        if ctx.author.id in nav:
-            pass
-        else:
-            await ctx.send("You are not allowed to use this command")
+        guild = self.client.get_guild(server_id)
+        roleNav = get(guild.roles, id=int(config.get("ROLES", "nav_role")))
+        if ctx.channel.id == int(config.get("CHANNELS", "send_updates")):
+            if roleNav in ctx.author.roles:
+                sfupdate_channel = self.client.get_channel(int(config.get("CHANNELS", "sector_file_update")))
+                try:
+                    await sfupdate_channel.send(args)
+                except Exception as e:
+                    await ctx.send(f"Error occured: {e}\nCould not post update")
+            else:
+                await ctx.send("You are not allowed to use this command")
     
     @commands.command(name="clear")
     @commands.has_permissions(manage_messages=True)
